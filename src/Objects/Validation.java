f474bcfda29fa1bea3b485aaf33b0d163c200dc8
charlessong0 <charlessong0@gmail.com>
2015-06-03 11:06:30 +0800

    validation

--- src/Objects/Validation.java
+++ src/Objects/Validation.java
@@ -1,5 +1,16 @@
 package Objects;
 
-public class Validation {
+import java.util.ArrayList;
 
+public class Validation {
+	private ArrayList<ColumnVal> columnList = new ArrayList<ColumnVal>();
+	
+	public void addColumn(ColumnVal column) {
+		columnList.add(column);
+	}
+	
+	//column start from 0
+	public ColumnVal getColumn(int i) {
+		return columnList.get(i);
+	}
 }
