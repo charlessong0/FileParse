75dcb9617d5381cd78e442cbd67cc65bfeeb8c02
charlessong0 <charlessong0@gmail.com>
2015-06-05 11:47:35 +0800

    merge testTable

--- src/Parser/TestTable.java
+++ src/Parser/TestTable.java
@@ -12,10 +12,10 @@ public class TestTable {
 	public String tableStr = "";
 	public String error = "";
 
-	public String gettableStr() throws Exception {
+	public String getTable() throws Exception {
 		//FileReader fr = new FileReader("C:\\Users\\znsong\\Documents\\My Received Files\\US-eBay-EOM-Fees@ebay.com.TRR-20140702.01.008.csv");
-		FileReader fr = new FileReader("C:\\Users\\znsong\\Documents\\My Received Files\\MVX5N5KZ9CTX8_results_2014-05-15-446.csv");
-		//FileReader fr = new FileReader("C:\\Users\\znsong\\Documents\\My Received Files\\US-eBay-EOM-Fees@ebay.com.TRR-20140702.01.008.csv");
+		//FileReader fr = new FileReader("C:\\Users\\znsong\\Documents\\My Received Files\\MVX5N5KZ9CTX8_results_2014-05-15-446.csv");
+		FileReader fr = new FileReader("C:\\Users\\znsong\\Documents\\My Received Files\\US-eBay-EOM-Fees@ebay.com.TRR-20140702.01.008.csv");
 		ReadXML readXML = new ReadXML("C:\\Users\\znsong\\Documents\\My Received Files\\structure.xml");
 		ArrayList<FileSample> fileList = readXML.getFileList();
 		FileSample file = fileList.get(0);
@@ -33,6 +33,13 @@ public class TestTable {
 		Structure structure = file.getStructure();
 		Table table = file.getTable();
 		boolean hasTitle = table.getHasTitle();
+		//String fileHeader = structure.getFileHeader();
+		//String fileFooter = structure.getFileFooter();
+		String batchHeader = structure.getBatchHeader();
+		String batchFooter = structure.getBatchFooter();
+		String title = structure.getTitle();
+		String content = structure.getContent();
+		ArrayList templateTitle = new ArrayList();
 		
 		//if there is existing title in the file, we can just use it and do not have to read title in XML template
 		if (hasTitle) {
@@ -46,14 +53,14 @@ public class TestTable {
 					Iterator<String> it = fr.fromCSVLinetoArray(readLine).iterator();
 					while (it.hasNext()) {
 						token = (String) it.next();
-						if (token.equals("CH")) {
+						if (token.equals(title)) {
 							show = true;
 							isTitle = true;
 							tableStr += "<thead>";
 							tableStr += "<tr>";
 							tableStr += "<th>Index</th>";
 						}
-						else if (token.equals("SB")) {
+						else if (token.equals(content)) {
 							show = true;
 							if (isTitle) {
 								isTitle = false;
@@ -67,7 +74,7 @@ public class TestTable {
 							tableStr += row;
 							tableStr += "</td>";
 						}
-						else if(token.equals("SF")) {
+						else if(token.equals(batchFooter)) {
 							tableStr += "</tr>";
 							show = false;
 							break;
@@ -77,6 +84,7 @@ public class TestTable {
 							if (row == 1 && column == 10) {
 								int upper = validation.getColumn(10).getUpper();
 								int lower = validation.getColumn(10).getLower();
+								//System.out.println(token);
 								int value = Integer.parseInt(token);
 								if (value < lower || value > upper) {
 									tableStr += "<td bgcolor=\"red\">";
@@ -117,15 +125,84 @@ public class TestTable {
 		}
 		//if there is no existing title in the file, we need to get the title in the XML template
 		else {
+			//add title in the table
+			tableStr += "<thead>";
+			tableStr += "<tr>";
+			tableStr += "<th>Index</th>";
+			Iterator itTitle = templateTitle.iterator();
+			while(itTitle.hasNext()) {
+				tableStr += "<th>";
+				tableStr += itTitle.next();
+				tableStr += "</th>";
+			}
+			tableStr += "</tr>";
+			tableStr += "</thead><tbody>";
 			
-			
-			
-			
+			while (true) {
+				readLine = fr.readLine();
+				lineLength = fr.fromCSVLinetoArray(readLine).size();
+				
+				if (lineLength == 0)
+					break;
+				else {
+					Iterator<String> it = fr.fromCSVLinetoArray(readLine).iterator();
+					while (it.hasNext()) {
+						token = (String) it.next();
+						if (token.equals(content)) {
+							show = true;
+							row ++;
+							column = 1;
+							tableStr += "</tr>";
+							tableStr += "<tr><td>";
+							tableStr += row;
+							tableStr += "</td>";
+						}
+						else if(token.equals(batchFooter)) {
+							tableStr += "</tr>";
+							show = false;
+							break;
+						}
+						else if (show) {
+							column++;
+							if (row == 1 && column == 10) {
+								int upper = validation.getColumn(10).getUpper();
+								int lower = validation.getColumn(10).getLower();
+								//System.out.println(token);
+								int value = Integer.parseInt(token);
+								if (value < lower || value > upper) {
+									tableStr += "<td bgcolor=\"red\">";
+									tableStr += token;
+									tableStr += "</td>";
+									error += "<div style=\"color:red\"><b>there is a validation conflict in row ";
+									error += row;
+									error += ", column ";
+									error += column;
+									error += "which is marked in red</b></div>";
+									System.out.println(error);
+								}
+								else {
+									tableStr += "<td>";
+									tableStr += token;
+									tableStr += "</td>";
+								}
+							}
+							else {
+								tableStr += "<td>";
+								tableStr += token;
+								tableStr += "</td>";
+							}
+						}
+					}
+					tableStr += "</tr>";
+				}
+				//System.out.println(tableStr);
+			}
 		}
 		
 		
 		//System.out.println(tableStr);
 		return tableStr;
 	}
+
 }
 
