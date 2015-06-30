7903820cfaf5f509a1ac4eaebff0489f333bb5ca
charlessong0 <charlessong0@gmail.com>
2015-06-30 09:11:16 +0800

    search util change

---
+++ src/control/SearchUtil.java
@@ -0,0 +1,12 @@
+package control;
+
+import java.util.ArrayList;
+
+public class SearchUtil {
+	public ArrayList<ArrayList<String>> searchBatch() {
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		
+		return result;
+	}
+
+}
